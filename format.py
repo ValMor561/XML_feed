90b9cd47a616f32239a155bea0f3110663297f71
ValMor561 <koli1babenko2@yandex.ru>
2024-03-27 10:03:12 +0300

    fix null value

--- format.py
+++ format.py
@@ -21,8 +21,15 @@ def get_house_building_id(house):
     else:
         return None
 
+def get_house_total_floor(house):
+    house_name_lower = str(house).lower()
+    matched_row = index_df[index_df['house'].str.lower() == house_name_lower]
+    if not matched_row.empty:
+        return matched_row.iloc[0]['floors-total']
+    else:
+        return None
 
-df = pd.read_excel("1.xlsx")
+df = pd.read_excel("data.xlsx", dtype=str)
 index_df = pd.read_excel('index.xlsx', dtype=str)
 df['image'] = df['Картинка для анонса']
 df['type'] = 'продажа'
@@ -45,7 +52,7 @@ df['yandex-house-id'] = df['address'].apply(house).apply(lambda x:  get_house_bu
 df['building-state'] = 'unfinished'
 df['price_value'] = df['Цена']
 df['price_currency'] = 'RUR'
-df['phone'] = '+7979147570727'
+df['phone'] = '+78005000941'
 df['organization'] = 'Группа компаний "ЛИГО"'
 df['url'] = 'https://ligo.group/'
 df['category'] = 'developer'
@@ -53,10 +60,11 @@ df['rooms'] = df['Число комнат']
 df['new-flat'] = 1
 df[['ready-quarter', 'built-year']] = df['Дата ввода'].str.split(' кв. ', expand=True)
 df['ready-quarter'] = df['ready-quarter'].apply(lambda x: from_roman(x) if pd.notna(x) else pd.NA)
-df['floors-total'] = ''
+df['floors-total'] = df['address'].apply(house).apply(lambda x:  get_house_total_floor(x))
 df['floor'] = df['Этаж']
 
 
     
 df.drop(columns=['Картинка для анонса', 'ID', 'Дата сдачи', 'Площадь', 'Полное название ЖК', 'Цена', 'Число комнат', 'Дата ввода', 'Этаж'], inplace=True)
+df = df.dropna(subset=['image', 'yandex-building-id', 'rooms'])
 df.to_excel('transformed_data.xlsx', index=False)
